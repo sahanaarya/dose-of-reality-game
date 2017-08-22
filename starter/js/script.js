51bd7ab0c9a14efef69a9656907df692f318c70f
Sahana Arya <sahana.arya@capitalone.com>
2017-08-22 15:20:07 -0400

    Elements add to body properly

--- starter/js/script.js
+++ starter/js/script.js
@@ -17,7 +17,6 @@ var letter;
 
 $('.name-section').hide();
 $('.drama-section').hide();
-$('.scenario-section').hide();
 $('.celeb-apprentice-section').hide();
 $('.kardashian-section').hide();
 $('.jersey-shore-section').hide();
@@ -62,42 +61,108 @@ $('.mexico-prompt').hide();
 $('.p-prompt').hide();
 $('.c-w-prompt').hide();
 
-var nameObject = {
-  question: "What is your name?",
-  placeholder: "Enter Name",
-  id: "name-input"
+var dramaObject = {
+  question: `Nice to meet you, ${name}. Hmmm... on a scale of 1-5 (1 being the least amount of drama and 5 being the most amount of drama), how much drama are you in the mood for today?`,
+  formClass:"drama-form",
+  divClass: "drama-section",
+  listItems: [1,2,3,4,5],
+  dropdownClass: "number"
+};
+
+var celebApprenticeObject = {
+  question: "Ooh, not in the mood for much drama today, are we? No worries, I hope you're in the mood for some challenges though. Let's get started. Choose a team - Blue or Red?",
+  formClass:"celeb-apprentice-form",
+  divClass: "celeb-apprentice-section",
+  listItems: ["Blue","Red"],
+  dropdownClass: "color"
+}
+
+var redObject = {
+  question: "So your project manager Ashton has proposed that you guys create either a minimalist logo, modern retro logo, or a bold avant-garde logo. Which one should you guys go with?",
+  formClass: "red-form",
+  divClass: "red-section",
+  listItems: ["Minimalist","Modern","Avant-Garde"],
+  dropdownClass: "design",
+  image: 'images/mtv.png',
+  imageClass: 'mtv',
+  promptStatement: "Great choice! Welcome to Celebrity Apprentice. The following stars are members of your team: Khloe Kardashian, Tom Brady, and Ashton Kutcher. Your task today is to design a new logo for the MTV VMA's.",
+  labelClass : "prompt"
+}
+
+var blueObject = {
+  question: "So your project manager Jay Z has proposed that you guys create either the world's spiciest burger or a bougie burger made with truffles and gold flakes. Which one type of burger should you guys go with?",
+  formClass: "blue-form",
+  divClass: "blue-section",
+  listItems: ["Bougie","Spicy"],
+  dropdownClass: "burger",
+  image: 'https://media0.giphy.com/media/O38MGj6hMuy6k/200.webp#2-grid1',
+  imageClass: 'shake-shack',
+  promptStatement: "Great choice! Welcome to Celebrity Apprentice. The following stars are members of your team: Lady Gaga, LeBron James, Jay Z. Your task today is to create a new burger to be featured on the Shake Shack menu.",
+  labelClass : "prompt"
+}
+
+var minimalistObject = {
+  text: "Ooh you are really not in the mood for drama today, huh? Minimalist designs are trendy but the MTV executives believe that in order to successfully market a music awards show to millenials, bolder is better. Nice try, but the Red Team must take an L :("
+}
+
+var modernObject = {
+  text: "Ooh you are really not in the mood for drama today, huh? Minimalist designs are trendy but the MTV executives believe that in order to successfully market a music awards show to millenials, bolder is better. Nice try, but the Red Team must take an L :("
+}
+
+var avantGardeObject = {
+  text: "Great choice! When it come to marketing the MTV VMAs, bolder is BETTER. It is a win for the Red Team!"
+}
+
+var bougieObject = {
+  text: "The Bougie Burger was a success! Such a success that Shake Shack has decided to make the Bougie Burger a permanent feature on their menu. Blue Team wins!"
+}
+
+var spicyObject = {
+  text: "Oh no :( You created the burger with good intentions, but unfortunately, the burger led to way too many health liabilities and went viral on Buzzfeed for this reason. Looks like the Blue Team took an L this time. :/"
 }
 
 function createForm(object){
-  var $formTag = $('<form>').addClass('form')
-  .append($('<div>'))
-  .append($('<label>'))
-  .append($('<input>').text(object.question))
+  var $formTag = $('<form>').addClass(object.formClass)
+  .append($('<img>').attr('src', object.image).addClass(object.imageClass))
+  .append($('<label>').addClass(object.labelClass).text(object.promptStatement))
+  .append($('<div>').addClass(object.divClass)
+  .append($('<label>').addClass('question').text(object.question)));
+
+  var $dropdown = $('<div>').addClass("dropdown")
+  .append($('<button>').addClass('btn btn-primary dropdown-toggle').attr('type','button').attr('data-toggle','dropdown').attr('id','team-dropdown').text('Options').append($('<span>').addClass('caret')));
+  var $ul = $('<ul>').addClass('dropdown-menu');
+  object.listItems.forEach(function(item){
+    var $li = $('<li>');
+    var $a = $('<a>').attr('href','#').html(item).addClass(object.dropdownClass);
+    $li.append($a);
+    $ul.append($li);
+    $dropdown.append($ul);
+  });
+  $formTag.append($dropdown);
   $('body').append($formTag);
+};
+
+function createPrompt(promptObject){
+  $labelTag = $('<label>').text(promptObject.text).addClass('result');
+  $('body').append($labelTag);
 }
-
-createForm(nameObject);
-
-console.log(nameObject.question);
-
-
 
 $('#start-button').on('click', function(){
   $('.name-section').fadeIn('slow');
 });
 
-
 $('.name-form').on('submit',function(e){
   e.preventDefault();
   name = $('#name-input').val();
-  $('.drama-section').fadeIn('slow');
-  $('.drama-section label').text(`Nice to meet you, ${name}. Hmmm... on a scale of 1-5 (1 being the least amount of drama and 5 being the most amount of drama), how much drama are you in the mood for today?`);
+  console.log(name);
+  createForm(dramaObject);
 });
 
-$('.number').on('click',function(){
+$('body').on('click',".number",function(){
   dramaLevel = $(this).html();
+  console.log(dramaLevel);
   if(dramaLevel<3){
-    $('.celeb-apprentice-section').fadeIn('slow');
+    createForm(celebApprenticeObject);
   } else if(dramaLevel==3){
     $('.kardashian-section').fadeIn('slow');
   } else{
@@ -106,37 +171,32 @@ $('.number').on('click',function(){
   }
 });
 
-$('.color').on('click', function(){
+$('body').on('click',".color", function(){
     team = $(this).html().toLowerCase();
     if(team=='red'){
-      $('.red-prompt').fadeIn('slow');
-      $('.red-section').fadeIn('slow');
-      $('.mtv').fadeIn('slow')
-
+      createForm(redObject);
     } else{
-      $('.blue-prompt').fadeIn('slow');
-      $('.blue-section').fadeIn('slow');
-      $('.shake-shack').fadeIn('slow');
+      createForm(blueObject);
     }
 });
 
-$('.design').on('click',function(){
+$('body').on('click','.design',function(){
   design = $(this).html().toLowerCase();
   if(design =='minimalist'){
-    $('.minimalist-prompt').fadeIn('slow');
+    createPrompt(minimalistObject);
   } else if(design == 'modern'){
-    $('.modern-prompt').fadeIn('slow');
+    createPrompt(modernObject);
   } else {
-    $('.avant-garde-prompt').fadeIn('slow');
+    createPrompt(avantGardeObject);
   }
 });
 
-$('.burger').on('click',function(){
+$('body').on('click',".burger",function(){
   burger = $(this).html().toLowerCase();
   if(burger =='bougie'){
-    $('.bougie-prompt').fadeIn('slow');
+    createPrompt('bougieObject');
   } else{
-    $('.spicy-prompt').fadeIn('slow');
+    createPrompt('spicyObject');
   }
 })
 
