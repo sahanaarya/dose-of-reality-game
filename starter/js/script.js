600a3f5ac918a3dd879d9914985aa51f437d2682
Sahana Arya <sahana.arya@capitalone.com>
2017-08-22 16:26:53 -0400

    Converted html content to jquery

--- starter/js/script.js
+++ starter/js/script.js
@@ -2,64 +2,8 @@
 console.log('JS Loaded');
 
 var name;
-var team;
-var dramaLevel;
-var design;
-var sister;
-var kimoji;
-var guest;
-var food;
-var choice;
-var guido;
-var destination;
-var letter;
-
 
 $('.name-section').hide();
-$('.drama-section').hide();
-$('.celeb-apprentice-section').hide();
-$('.kardashian-section').hide();
-$('.jersey-shore-section').hide();
-$('.js-logo').hide();
-$('.red-prompt').hide();
-$('.red-section').hide();
-$('.mtv').hide()
-$('.blue-prompt').hide();
-$('.blue-section').hide();
-$('.shake-shack').hide();
-$('.minimalist-prompt').hide();
-$('.modern-prompt').hide();
-$('.avant-garde-prompt').hide();
-$('.bougie-prompt').hide();
-$('.spicy-prompt').hide();
-$('.kim-section').hide();
-$('.kim-face').hide();
-$('.khloe-section').hide();
-$('.khloe-gif').hide();
-$('.kourtney-section').hide();
-$('.kourt-kit-kat').hide();
-$('.3d-prompt').hide();
-$('.kimye-prompt').hide();
-$('.holographic-prompt').hide();
-$('.chyna-prompt').hide();
-$('.kris-prompt').hide();
-$('.tyga-prompt').hide();
-$(".oreos-reeses-prompt").hide();
-$('.pop-tarts-prompt').hide();
-$('.snooki-section').hide();
-$('.snooki-gif').hide();
-$('.jwoww-section').hide();
-$('.jwoww-gif').hide();
-$('.mike-section').hide();
-$('.situation-gif').hide();
-$('.bail-prompt').hide();
-$('.lesson-prompt').hide();
-$('.laugh-prompt').hide();
-$('.spain-prompt').hide();
-$('.bahamas-prompt').hide();
-$('.mexico-prompt').hide();
-$('.p-prompt').hide();
-$('.c-w-prompt').hide();
 
 var dramaObject = {
   question: `Nice to meet you, ${name}. Hmmm... on a scale of 1-5 (1 being the least amount of drama and 5 being the most amount of drama), how much drama are you in the mood for today?`,
@@ -75,6 +19,21 @@ var celebApprenticeObject = {
   divClass: "celeb-apprentice-section",
   listItems: ["Blue","Red"],
   dropdownClass: "color"
+}
+
+var kardashianObject = {
+  question: "So you want a happy medium, huh? Well, congratulations - you are now have a role in Keeping Up With the Kardashians! In this week's episode you will be featured as the BFF of one of the sisters. Which sister will it be?",
+  formClass:"kardashian-form",
+  divClass: "kardashian-section",
+  listItems: ["Kourtney","Kim", "Khloe"],
+  dropdownClass: "sister"
+}
+
+var jerseyshoreObject = {
+  question: "We're definitely turning up the drama today for you! Congratulations, you are now officially the newest cast member of the Jersey Shore. In the upcoming episode, you will be introduced as the BFF of one of the current castmates. Which castmate will you pick? Your choices are Snooki, JWoww, and The Situation. Which one will you pick?",
+  divClass: "jersey-shore-section",
+  listItems: ["Snooki","JWoww", "Situation"],
+  dropdownClass: "guido"
 }
 
 var redObject = {
@@ -95,7 +54,7 @@ var blueObject = {
   divClass: "blue-section",
   listItems: ["Bougie","Spicy"],
   dropdownClass: "burger",
-  image: 'https://media0.giphy.com/media/O38MGj6hMuy6k/200.webp#2-grid1',
+  image: 'images/burger-gif.webp',
   imageClass: 'shake-shack',
   promptStatement: "Great choice! Welcome to Celebrity Apprentice. The following stars are members of your team: Lady Gaga, LeBron James, Jay Z. Your task today is to create a new burger to be featured on the Shake Shack menu.",
   labelClass : "prompt"
@@ -119,6 +78,130 @@ var bougieObject = {
 
 var spicyObject = {
   text: "Oh no :( You created the burger with good intentions, but unfortunately, the burger led to way too many health liabilities and went viral on Buzzfeed for this reason. Looks like the Blue Team took an L this time. :/"
+}
+
+var kimObject = {
+  question: "Kim needs help coming up with an idea for a new collection of Kimojis. So far, she has three ideas in mind and is relying on you to select one. The options are 3D kimojis, kimye kimojis, and holographic kimojis. Which one will you pick?",
+  formClass: "kim-form",
+  divClass: "kim-section",
+  listItems: ["3D","Kimye","Holographic"],
+  dropdownClass: "kimoji",
+  image: 'images/kim.webp',
+  imageClass: 'kim-face'
+}
+
+var threedObject = {
+  text: "Congrats! The 3D Kimojis were a success. In fact the Kimoji app developers are now working on rolling out a collection of 4D Kimojis due to the 3D collection's enormous success. Plus, you get a cut of the profits. Cha-ching!"
+}
+
+var kimyeObject = {
+  text: "Ehh, it looks like the public is over the Kimye hype, and the new collection did not perform as well as you expected. Unfortunately, it looks like this footage will not be featured on the show - sorry! :("
+}
+
+var holographicObject = {
+  text: "Awesome choice. News of Holographic Kimojis went viral. In fact, you played such an instrumental role in the success of the collection, that they have decided to feature you in the next collection of Kimojis. Congrats on your big break!"
+}
+
+var khloeObject = {
+  question: "Arguably, the most fun and down-to-earth sister, Khloe is excited to have you featured on the show! For the first order of business, Khloe needs your help choosing which celebrity to interview on her talk show 'Kocktails with Khloe'. She has narrowed her list down to a few famous Kardashian exes: Blac Chyna, Kris Humphries, and Tyga. Who should she choose to interview?",
+  formClass: "khloe-form",
+  divClass: "khloe-section",
+  listItems: ["Chyna","Kris","Tyga"],
+  dropdownClass: "guest",
+  image: 'images.khloe.gif',
+  imageClass: 'khloe-gif'
+}
+
+var chynaObject = {
+  text:"Ooh yes... looks like you found a way to turn up the drama! Khloe's interview with Chyna was arguably the most successful episode of Kocktails with Khloe. The show has never been more popular and has been renewed for another season. Congrats!!"
+}
+
+var krisObject = {
+  text: "It looks like the public is over Kim's '72-day marriage'. Fewer people chose to tune into the show and the show suffered poor ratings. It's up to you to find a way for Khloe to make a comeback!!"
+}
+
+var tygaObject = {
+  text: "Tyga.... really? Turns out that Kylie got word of the decision to interview Tyga and is refusing to speak to Khloe :/ It's up to you to patch things up!"
+}
+
+var kourtneyObject = {
+  question: "Have you seen that viral video of Kourtney teaching the world how to eat a Kit-Kat? Well, it looks like she is making a sequel but needs to choose another type of food. So far, she is considering either Oreos, Pop Tarts, or Reese's Peanut Butter cups. Which one should she feature in her next video?",
+  formClass: "kourtney-form",
+  divClass: "kourtney-section",
+  listItems: ["Oreos","Pop Tarts","Reese's"],
+  dropdownClass: "food",
+  image: 'images/kourtney.gif',
+  imageClass: 'kourt-kit-kat'
+}
+
+var oreosReesesObject = {
+  text: "Uh oh, it looks like your choice of food was too controversial. Turns out that people do not want instruction on how to eat their favorite childhood sweets. For this reason, there has actually been an increase in the number of people unsubscribing from Kourtney's app. :("
+}
+
+var poptartsObject = {
+  text: "Not a bad choice! In fact, you had the foresight to take it one step further and get the video sponsored by the creators of Pop Tarts. Your brilliance will be rewarded by a share of profits from this soon-to-be viral video. Congrats!!"
+}
+
+var snookiObject = {
+  question: "Bold move. On your first day of taping the show, Snooki went on a wild adventure and found herself in jail. Your options are to: bail her out, let her learn her lesson, or point and laugh. What will you do?",
+  formClass: "snooki-form",
+  divClass: "snooki-section",
+  listItems: ["Bail","Lesson","Laugh"],
+  dropdownClass: "choice",
+  image: 'images/snooki.webp',
+  imageClass: 'snooki-gif'
+}
+
+var bailObject = {
+  text: "You are a kind soul. Unfortunately, Snooki didn't learn her lesson and ended up in jail the very next day. I guess now it's time to show some tough love."
+}
+
+var lessonObject = {
+  text: "Solid choice. However, somehow Snooki managed to escape from jail and is angry at you for not bailing her out when you had the chance. I'd watch my back if I were you...."
+}
+
+var laughObject = {
+  text: "LOL. You win, game over."
+}
+
+var jwowwObject = {
+  question: "So, I'm gonna let you in on a little secret... it turns out that JWoww is actually the brains behind the whole show. At the moment, one of her responsibilities is to pick an international destination to shoot the next season. So far, the options are Spain, Bahamas, and Mexico. Which one should she choose?",
+  formClass: "jwoww-form",
+  divClass: "jwoww-section",
+  listItems: ["Spain","Bahamas","Mexico"],
+  dropdownClass: "destination",
+  image: 'images/jwoww.gif',
+  imageClass: 'jwoww-gif'
+}
+
+var spainObject = {
+  text: "Good choice, but the rest of the cast is not too psyched about it... In fact, on the first day of shooting, they decide to ditch and catch a plane to Italy instead. It's up to you to hunt them down and bring them back to the shooting location!"
+}
+
+var bahamasObject = {
+  text: "Good choice, but the rest of the cast is not too psyched about it... In fact, on the first day of shooting, they decide to ditch and catch a plane to Mexico instead. It's up to you to hunt them down and bring them back to the shooting location!"
+}
+
+var mexicoObject = {
+  text: "Perfect choice! The new season was a hit and they want to bring you on board as a full-time location consultant. Get psyched because it pays $200K/year. Cha ching!"
+}
+
+var mikeObject = {
+  question: "So 'The Situation' has a brilliant idea to add one more letter to the famous Jersey Shore Acronym 'GTL', and he needs your help. What letter should be added on? The options are P for Party, C for Chill, and W for Work. Which one should he pick?",
+  formClass: "mike-form",
+  divClass: "mike-section",
+  listItems: ["P","C","W"],
+  dropdownClass: "letter",
+  image: 'images/situation.webp',
+  imageClass: 'situation-gif'
+}
+
+var pObject = {
+  text: "You nailed it! 'GTL' is now 'GTLP' (Gym, Tan, Laundry, Party) and a trending hashtag on Twitter. Nice job!"
+}
+
+var cwObject = {
+  text: "Eh, unfortunately the new extended acronym didn't successfully catch on, so the Jersey Shore crew is reverting back to the OG catchphrase 'GTL'. Better luck next time!"
 }
 
 function createForm(object){
@@ -153,21 +236,18 @@ $('#start-button').on('click', function(){
 
 $('.name-form').on('submit',function(e){
   e.preventDefault();
-  name = $('#name-input').val();
   console.log(name);
   createForm(dramaObject);
 });
 
 $('body').on('click',".number",function(){
   dramaLevel = $(this).html();
-  console.log(dramaLevel);
   if(dramaLevel<3){
     createForm(celebApprenticeObject);
   } else if(dramaLevel==3){
-    $('.kardashian-section').fadeIn('slow');
+    createForm(kardashianObject);
   } else{
-    $('.jersey-shore-section').fadeIn('slow');
-    $('.js-logo').fadeIn('slow');
+    createForm(jerseyshoreObject);
   }
 });
 
@@ -200,94 +280,88 @@ $('body').on('click',".burger",function(){
   }
 })
 
-$('.sister').on('click',function(){
+$('body').on('click',".sister",function(){
   sister = $(this).html().toLowerCase();
   if(sister == 'kim'){
-    $('.kim-section').fadeIn('slow');
-    $('.kim-face').fadeIn('slow');
+    createForm(kimObject);
   } else if(sister == 'khloe'){
-    $('.khloe-section').fadeIn('slow');
-    $('.khloe-gif').fadeIn('slow');
+    createForm(khloeObject);
   } else if(sister == 'kourtney'){
-    $('.kourtney-section').fadeIn('slow');
-    $('.kourt-kit-kat').fadeIn('slow');
+    createForm(kourtneyObject);
   }
 });
 
-$('.kimoji').on('click',function(){
+$('body').on('click',".kimoji",function(){
   kimoji = $(this).html().toLowerCase();
   if(kimoji == '3d'){
-    $('.3d-prompt').fadeIn('slow');
+    createPrompt(threedObject);
   } else if(kimoji == 'kimye'){
-    $('.kimye-prompt').fadeIn('slow');
+    createPrompt(kimyeObject);
   } else{
-    $('.holographic-prompt').fadeIn('slow');
+    createPrompt(holographicObject);
   }
   });
 
-  $('.guest').on('click', function(){
+  $('body').on('click','.guest', function(){
     guest = $(this).html().toLowerCase();
     if(guest == 'chyna'){
-      $('.chyna-prompt').fadeIn('slow');
+      createPrompt(chynaObject);
     } else if(guest == 'kris'){
-      $('.kris-prompt').fadeIn('slow');
+      createPrompt(krisObject);
     } else{
-      $('.tyga-prompt').fadeIn('slow');
+      createPrompt(tygaObject);
     }
   })
 
-  $('.food').on('click',function(){
+  $('body').on('click',".food",function(){
     food = $(this).html().toLowerCase();
     console.log(food);
     if(food == 'oreos' || food == "reese's"){
-      $(".oreos-reeses-prompt").fadeIn('slow');
+      createPrompt(oreosReesesObject);
     } else{
-      $('.pop-tarts-prompt').fadeIn('slow');
+      createPrompt(poptartsObject);
     }
   })
 
-  $('.guido').on('click',function(){
+  $('body').on('click','.guido',function(){
     guido = $(this).html().toLowerCase();
     if(guido == 'snooki'){
-      $('.snooki-section').fadeIn('slow');
-      $('.snooki-gif').fadeIn('slow');
+      createForm(snookiObject);
     } else if(guido == 'jwoww'){
-      $('.jwoww-section').fadeIn('slow');
-      $('.jwoww-gif').fadeIn('slow');
+      createForm(jwowwObject);
     } else{
-      $('.mike-section').fadeIn('slow');
-      $('.situation-gif').fadeIn('slow');
+      createForm(mikeObject);
     }
   })
 
-  $('.choice').on('click',function(){
+  $('body').on('click','.choice',function(){
     choice = $(this).html().toLowerCase();
     if(choice == "bail"){
-      $('.bail-prompt').fadeIn('slow');
+      createPrompt(bailObject);
     } else if(choice == "lesson"){
-      $('.lesson-prompt').fadeIn('slow');
+      createPrompt(lessonObject);
     } else{
-      $('.laugh-prompt').fadeIn('slow');
+      createPrompt(laughObject);
     }
   })
 
-  $('.destination').on('click',function(){
+  $('body').on('click','.destination',function(){
     destination = $(this).html().toLowerCase();
     if(destination == "spain"){
-      $('.spain-prompt').fadeIn('slow');
+      createPrompt(spainObject);
     } else if(destination == "bahamas"){
-      $('.bahamas-prompt').fadeIn('slow');
+      createPrompt(bahamasObject);
     } else{
-      $('.mexico-prompt').fadeIn('slow');
+      createPrompt(mexicoObject);
     }
   })
 
-  $('.letter').on('click',function(){
+  $('body').on('click','.letter',function(){
     letter = $(this).html().toLowerCase();
     if(letter == "p"){
-      $('.p-prompt').fadeIn('slow');
+      createPrompt(pObject);
     } else{
-      $('.c-w-prompt').fadeIn('slow');
+      createPrompt(cwObject);
     }
   });
 
